248eb821f00c4c9d2a3366697c493934f01a40b8
JIN <jinorabel@gmail.com>
2024-04-03 16:57:49 +0900

    class : react-app 생성 및 컴포넌트

---
+++ 09_react_component/src/components/FunctionComponent.js
@@ -0,0 +1,12 @@
+// function FunctionComponent() {
+//   return <div>함수형 컴포넌트</div>;
+// }
+
+// export default FunctionComponent;
+
+// rfc + enter or tab : 파일명과 동일한 이름의 함수형 컴포넌트 만들어줌
+import React from 'react';
+
+export default function FunctionComponent() {
+  return <div>FunctionComponent</div>;
+}
