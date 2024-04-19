70b6aa34cf90040af1041b44573eb00039086772
JIN <jinorabel@gmail.com>
2024-04-19 10:20:29 +0900

    class : react event

---
+++ 12_react_event/src/App.js
@@ -0,0 +1,21 @@
+import FuncEvent from './components/FuncEvent';
+import ClassEvent from './components/ClassEvent';
+import HandleEx from './components/ex/HandleEx';
+import Prac2 from './components/ex/Prac2';
+import Prac3 from './components/ex/Prac3';
+
+function App() {
+  return (
+    <>
+      <FuncEvent />
+      <ClassEvent />
+
+      <HandleEx />
+
+      <Prac2 />
+      <Prac3 />
+    </>
+  );
+}
+
+export default App;
