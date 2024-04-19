7d9bae2f66484ed278165b83a0fd851e3f324964
JIN <jinorabel@gmail.com>
2024-04-19 10:20:50 +0900

    class : react ref

---
+++ 13_react_ref/src/App.js
@@ -0,0 +1,17 @@
+import FuncRef from './components/FuncRef';
+import ClassRef from './components/ClassRef';
+import RefPrac1 from './components/RefPrac1';
+
+function App() {
+  return (
+    <>
+      <FuncRef />
+
+      <ClassRef />
+
+      <RefPrac1 />
+    </>
+  );
+}
+
+export default App;
